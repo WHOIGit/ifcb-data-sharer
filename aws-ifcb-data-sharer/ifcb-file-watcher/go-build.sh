65c7ed3f9f116e93f508f4c5c9c045c38c8c3fe4
ethanandrews <eandrews@whoi.edu>
2025-01-14 11:10:27 -0500

    add sync function to wrapper script, add mac OS version

--- aws-ifcb-data-sharer/ifcb-file-watcher/go-build.sh
+++ aws-ifcb-data-sharer/ifcb-file-watcher/go-build.sh
@@ -4,3 +4,6 @@ GOOS=linux GOARCH=amd64 go build -o ifcb-file-watcher
 
 # Windows compile
 GOOS=windows GOARCH=amd64 go build -o ifcb-file-watcher-windows
+
+# Mac compile
+GOOS=darwin GOARCH=arm64 go build -o ifcb-file-watcher-macos
